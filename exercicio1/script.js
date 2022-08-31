00b8423e945cbcf23f2c41427cb9f52ef18b50b6
renanpmiranda <renanpmiranda5@gmail.com>
2022-08-31 13:57:13 -0300

    a009-exercicio01

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+const nomeDoUsuario = prompt("Qual o seu nome?")
+const comida1 = prompt("Qual a sua comida favorita?")
+const comida2 = prompt("Qual a sua segunda comida favorita?")
+const comida3 = prompt("Qual a sua terceira comida favorita?")
+
+console.log(`Estas são as comidas favoritas de ${nomeDoUsuario}:
+- ${comida1}
+- ${comida2}
+- ${comida3}
+`)
