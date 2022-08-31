487cba66fea70b9da739e646915604c89985ca2c
renanpmiranda <renanpmiranda5@gmail.com>
2022-08-31 14:33:25 -0300

    a009-exercicio03

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,8 @@
-//Crie a const para a frase aqui
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres:\"BOAS VINDAS, mas não deixe o gato sair\""
+
+const novaFrase = frase.replace("verde", "rosa")//.replace("azul", "laranja")
+const novaFrase2 = novaFrase.replace("azul", "laranja")
+
+console.log(novaFrase2)
+console.log(novaFrase2.includes("verde" && "laranja"))
+console.log(novaFrase2.replace("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR"))
